db16ab1fdc3373e5564a5207693ab07a5dbb54e3
uswanadeem09675 <wtsplayes001@gmail.com>
2023-04-13 00:03:33 +0500

    design_update

--- src/Components/Pages/Detailspage/Detailspage.jsx
+++ src/Components/Pages/Detailspage/Detailspage.jsx
@@ -1,22 +1,22 @@
-import React, {useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import YouTube from '@u-wave/react-youtube';
 let videoIdList = ["AOMpxsiUg2Q", "XM-HJT8_esM", "r2pdwduPS5U"];
 
 export default function Detailspage() {
     const [videoId, setVideoId] = useState(videoIdList[0])
     const a = useRef(0);
-    const option = {
-        height: '390',
-        width: '640',
-        playerVars: {
-            autoplay: 1,
-        },
-    };
-    
+    // const option = {
+    //     height: '390',
+    //     width: '640',
+    //     playerVars: {
+    //         autoplay: 1,
+    //     },
+    // };
+
     const handleEnd = () => {
         setVideoId(videoIdList[++a.current]);
     };
-    
+
 
     const onReady = (event) => {
         // access to player in all event handlers via event.target
@@ -27,12 +27,14 @@ export default function Detailspage() {
             <section className=' layout_padding' >
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-7 ">
-                            <h2 className="main-heading  ">
-                                {/* Video Title */}
-                            </h2>
+                        <h2 className="main-heading  ">
+                            Video Title
+                        </h2>
+                        <div className="col-lg-7 mt-5 ">
+
                             <YouTube video={videoId}
-                                opts={option}
+                                width={600}
+                                height={400}
                                 onEnd={handleEnd}
                                 onReady={onReady}
                             />
@@ -42,7 +44,7 @@ export default function Detailspage() {
                                 videoIdList.map((item) => {
                                     return (
                                         <div className="card d-flex m-2" style={{ flexDirection: "row" }} onClick={() => {
-                                            setVideoId(item.videoId)
+                                            setVideoId(item.videoIdList)
                                         }}>
                                             <img className="w-25" src={"http://img.youtube.com/vi/" + item.videoId + "/0.jpg"} />
                                             <p className='p-2'>{item.videotitle}</p>
